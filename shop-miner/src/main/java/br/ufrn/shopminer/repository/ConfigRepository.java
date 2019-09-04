150e6d3bc2f761b45fc9dd2b63b5564524cc5679
Felipe Morais <moraisfelipe97@gmail.com>
2019-09-04 01:42:50 -0300

    Revert "restControllers"
    This reverts commit d0b41f266b4206b3c1440718cfd4d8a0d26a1926.

--- shop-miner/src/main/java/br/ufrn/shopminer/repository/ConfigRepository.java
+++ shop-miner/src/main/java/br/ufrn/shopminer/repository/ConfigRepository.java
@@ -1,5 +1,7 @@
 package br.ufrn.shopminer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import br.ufrn.shopminer.model.Config;
 @Repository
 public interface ConfigRepository extends JpaRepository<Config, Integer> {
 
-	Config findById(int id);
+	Optional<Config> findById(Integer id);
 
 }
--- shop-miner/src/main/java/br/ufrn/shopminer/repository/SiteRepository.java
+++ shop-miner/src/main/java/br/ufrn/shopminer/repository/SiteRepository.java
@@ -1,5 +1,7 @@
 package br.ufrn.shopminer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import br.ufrn.shopminer.model.Site;
 @Repository
 public interface SiteRepository extends JpaRepository<Site, Integer> {
 
+	//Optional<Site> findById(Integer id);
 	Site findById(int id);
 
 }
