d24e9525fda7f61130cf7d41e9be06ad8b54e1d3
Felipe Morais <moraisfelipe97@gmail.com>
2019-10-08 01:26:31 -0300

    saddsaasd

--- shop-miner/src/main/java/br/ufrn/shopminer/controller/PredictorController.java
+++ shop-miner/src/main/java/br/ufrn/shopminer/controller/PredictorController.java
@@ -6,13 +6,17 @@ import br.ufrn.shopminer.service.PredictorService;
 import br.ufrn.shopminer.service.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import org.netlib.util.doubleW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @RestController
@@ -22,25 +26,79 @@ import java.util.List;
 public class PredictorController {
     @Autowired
     private PredictorService predictorService;
+    
+    @Autowired
     private FavoriteService favoriteService;
+    
+    @Autowired
     private ProductService productService;
 
-    @GetMapping("/{fav}/{date}") //
+    @GetMapping("/predict/{fav}/{mi}/{yi}/{me}/{ye}") //
     @ApiOperation(value = "Returns an object with best price")
     public ResponseEntity<Double> getPredictions(
-            @PathVariable("id") Integer fav,
-            @PathVariable("initial-date") String initDate,
-            @PathVariable("final-date") String finalDate) {
+            @PathVariable("fav") Integer fav,
+            @PathVariable("mi") Integer mi,
+            @PathVariable("yi") Integer yi,
+            @PathVariable("me") Integer me,
+            @PathVariable("ye") Integer ye
+            ) {
         ResponseEntity<Double> re;
 
+        double best = 999999;
+        Date bestDate = new Date();
+        Site bestSite = new Site();
+        	
         try {
             Favorite favorite = favoriteService.findOne(fav).get();
             List<Site> sites = favorite.getConfig().getSites();
-            List<Price> prices =  favoriteService.findPrices(sites.get(0).getId(),fav);
-            Date start = new Date(initDate);
-            predictorService.train(prices, 5);
-            double best = predictorService.predict(start);
+            
+          
+            Date start = new Date();
+            start.setMonth(2);
+            start.setYear(yi);
 
+            
+            Date end = new Date();
+            start.setMonth(9);
+            start.setYear(ye);
+            
+            
+            
+            for (Site site : sites) {
+            	List<Price> prices =  favoriteService.findPrices(fav, site.getId());
+            	predictorService.train(prices, 5);
+            	
+            	Date aux = new Date();
+                start.setMonth(start.getMonth());
+                start.setYear(start.getYear());
+                
+                System.out.println(start);
+                System.out.println(end);
+                
+                while (aux.before(end)) {
+                	System.out.println("bef " + aux);
+                	double pred = predictorService.predict(start);
+                	
+                	if (pred < best) {
+                		best = pred;
+                		bestDate = new Date();
+                		bestDate.setMonth(aux.getMonth());
+                		bestDate.setYear(aux.getYear());
+                		bestSite = site;
+                	}
+                	
+                	aux.setMonth(aux.getMonth()+1);
+                }
+            	
+                
+            }
+            
+            
+            System.out.println(best);
+            System.out.println(bestDate);
+            System.out.println(bestSite.getName());
+            
+            
             //SiteProductPrice best = new SiteProductPrice();
             //Price bestPrice = new Price();
             //Favorite favorite = favoriteService.findOne(fav).get();
--- shop-miner/src/main/java/br/ufrn/shopminer/service/PredictorService.java
+++ shop-miner/src/main/java/br/ufrn/shopminer/service/PredictorService.java
@@ -1,24 +1,32 @@
 package br.ufrn.shopminer.service;
 
 import br.ufrn.shopminer.model.Price;
-import br.ufrn.shopminer.strategies.LinearRegression;
 import br.ufrn.shopminer.strategies.PolynomialMonthRegression;
 import br.ufrn.shopminer.strategies.Strategy;
 
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Collection;
 import java.util.Date;
 
+@Service
+@Transactional(readOnly = true)
 public class PredictorService {
     private Strategy strategy;
 
     public PredictorService() {
         this.strategy = new PolynomialMonthRegression();
     }
+    
+    @Transactional(readOnly = false)
     public double predict(Date date) {
         return strategy.predict(date);
     }
 
+    @Transactional(readOnly = false)
     public void setStrategy(String strategy) {
 
         if (strategy.toLowerCase().equals("polynomial month regression")) {
@@ -29,6 +37,7 @@ public class PredictorService {
         }
     }
 
+    @Transactional(readOnly = false)
     public void train(List<Price> prices, int degree) {
         this.strategy.train(prices, degree);
     }
