cbbad59d885045e22aa2cf448a4dec4c652c81df
Felipe Morais <moraisfelipe97@gmail.com>
2019-10-08 01:56:21 -0300

    separado predict controller e service

--- shop-miner/src/main/java/br/ufrn/shopminer/service/PredictorService.java
+++ shop-miner/src/main/java/br/ufrn/shopminer/service/PredictorService.java
@@ -1,11 +1,15 @@
 package br.ufrn.shopminer.service;
 
+import br.ufrn.shopminer.model.Favorite;
 import br.ufrn.shopminer.model.Price;
+import br.ufrn.shopminer.model.Site;
 import br.ufrn.shopminer.strategies.PolynomialMonthRegression;
 import br.ufrn.shopminer.strategies.Strategy;
+import java_cup.internal_error;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +20,12 @@ import java.util.Date;
 @Transactional(readOnly = true)
 public class PredictorService {
     private Strategy strategy;
+    
+    @Autowired
+    private FavoriteService favoriteService;
+    
+    @Autowired
+    private ProductService productService;
 
     public PredictorService() {
         this.strategy = new PolynomialMonthRegression();
@@ -41,4 +51,59 @@ public class PredictorService {
     public void train(List<Price> prices, int degree) {
         this.strategy.train(prices, degree);
     }
+    
+    @Transactional(readOnly = false)
+    public String getPredictions(Integer fav, 
+    		                     Integer di, Integer mi, Integer yi,
+    		                     Integer de, Integer me, Integer ye) {
+    	
+    	double best = 999999;
+        Date bestDate = new Date();
+        Site bestSite = new Site();
+        
+        String result = "";
+        
+        Favorite favorite = favoriteService.findOne(fav).get();
+        List<Site> sites = favorite.getConfig().getSites();
+        
+      
+        Date start = new Date(yi, mi, di);            
+        Date end = new Date(ye, me, de);
+
+        for (Site site : sites) {
+        	List<Price> prices =  favoriteService.findPrices(fav, site.getId());
+        	train(prices, 5);
+        	
+        	Date aux = new Date();
+            aux.setMonth(start.getMonth());
+            aux.setYear(start.getYear());
+            
+            
+            while (aux.before(end)) {
+            	double pred = predict(aux);
+            	
+            	if (pred < best) {
+            		best = pred;
+            		bestDate = new Date();
+            		bestDate.setMonth(aux.getMonth());
+            		bestDate.setYear(aux.getYear());
+            		bestSite = site;
+            	}
+            	
+            	aux.setMonth(aux.getMonth()+1);
+            }
+        	
+            
+        }
+        
+        
+
+        String dateString = bestDate.getDay() + "-" + (bestDate.getMonth()+1) + "-" + bestDate.getYear();
+        result = best + "|" + dateString + "|" + bestSite.getName();
+        
+        return result;
+    	
+    }
+    
+    
 }
