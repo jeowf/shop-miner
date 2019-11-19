b44221c47b08b540f7d7f4ca1f98a0d3118b7bd4
JoMedeiros <josivan.medeiros@live.com>
2019-11-19 00:50:09 -0300

    QueryFactory

---
+++ shop-miner/src/main/java/br/ufrn/shopminer/core/Attribute.java
@@ -0,0 +1,22 @@
+package br.ufrn.shopminer.core;
+
+public class Attribute<T> {
+    private String name;
+    private T value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
