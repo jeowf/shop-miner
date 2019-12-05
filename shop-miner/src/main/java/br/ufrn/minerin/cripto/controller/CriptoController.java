84dd69861fb966104086f5b9ae2a2f016b8f7b14
Felipe Morais <moraisfelipe97@gmail.com>
2019-12-05 00:20:15 -0300

    80%

--- shop-miner/src/main/java/br/ufrn/minerin/cripto/controller/CriptoController.java
+++ shop-miner/src/main/java/br/ufrn/minerin/cripto/controller/CriptoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,22 @@ public class CriptoController  {
 		try {
 			coins = priceService.findAll();
 			re = new ResponseEntity<> (coins, HttpStatus.OK);
+		} catch (Exception e) {
+			re = new ResponseEntity<> (null, HttpStatus.NOT_FOUND);
+		}
+
+		return re;
+	}
+	
+	@GetMapping("/coin/{id}")
+	@ApiOperation(value = "Returns a Coin by id")
+	public ResponseEntity<Coin> getCoin(@PathVariable("id") Integer id) {
+		Coin coin;
+		ResponseEntity<Coin> re;
+		
+		try {
+			coin = priceService.findOne(id).get();
+			re = new ResponseEntity<> (coin, HttpStatus.FOUND);
 		} catch (Exception e) {
 			re = new ResponseEntity<> (null, HttpStatus.NOT_FOUND);
 		}
--- shop-miner/src/main/java/br/ufrn/minerin/cripto/service/custom/CriptoPersistStrategy.java
+++ shop-miner/src/main/java/br/ufrn/minerin/cripto/service/custom/CriptoPersistStrategy.java
@@ -1,14 +1,25 @@
 package br.ufrn.minerin.cripto.service.custom;
 
+import br.ufrn.minerin.cripto.model.Coin;
+import br.ufrn.minerin.cripto.model.MPrice;
 import br.ufrn.minerin.cripto.model.RTPrice;
+import br.ufrn.minerin.cripto.service.CoinService;
+import br.ufrn.minerin.cripto.service.MPriceService;
 import br.ufrn.minerin.cripto.service.RTPriceService;
 import br.ufrn.minerin.framework.service.core.PersistStrategy;
+
+import org.netlib.util.floatW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Primary
@@ -16,14 +27,64 @@ public class CriptoPersistStrategy implements PersistStrategy {
 
 	@Autowired
 	private RTPriceService rtPriceService;
+	
+	@Autowired
+	private MPriceService mPriceService;
+	
+	@Autowired
+	private CoinService coinService;
 
 	@Override
 	public void persistQueries(List<Serializable> queries) {
 
+		int ce = 10;
+		
 		try {
-			for(Serializable query : queries){
-				rtPriceService.save((RTPrice) query);
+			List<RTPrice> rtps = rtPriceService.findAll();
+			
+			if (rtps != null && rtps.size() > 5*10) {
+				
+				float mean = 0;
+				int qtt = 0;
+				
+				Map<Coin, Float> means = new HashMap<Coin, Float>();
+				
+				for (RTPrice r : rtps) {
+					if (!means.containsKey(r.getCoin())) {
+						means.put(r.getCoin(), (float) 0);
+					} 
+					means.put(r.getCoin(), (means.get(r.getCoin()) + r.getValue())/ce );
+					rtPriceService.delete(r);
+				}
+				
+				List<Coin> coins = coinService.findAll();
+				
+				for (Coin coin : coins) {
+					float v = means.get(coin);
+					MPrice m = new MPrice();
+					m.setCoin(coin);
+					
+					Timestamp ts = new Timestamp(System.currentTimeMillis());
+					Calendar ca = Calendar.getInstance();
+					Date date=new Date(ts.getTime());
+					ca.setTime(date);
+					
+					m.setTimestamp(ca.getTime());
+					m.setValue(v);
+					
+					mPriceService.save(m);
+				}
+				
+
+				
+				mean /= qtt;
+			} else {
+				for(Serializable query : queries){
+					rtPriceService.save((RTPrice) query);
+				}
 			}
+			
+			
 		}catch(Exception e) {
 			System.out.println("Operação Inválida");
 			e.printStackTrace();
